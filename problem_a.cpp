5c0a715dbe76e0728a7f9142ab8b23934b0322a6
Louw, Edward <louw.edward.usi@gmail.com>
2016-04-09 02:05:27 -0400

    Submission for problem A small/large.

--- problem_a.cpp
+++ problem_a.cpp
@@ -17,27 +17,33 @@ int getNumDigits(int n) {
     return retVal;
 }
 
-int main() {
+void printOutput(int numCases, int* solutions) {
+    for(int i = 0; i < numCases; i++) {
+        cout << "Case #" << i+1 << ": ";
+        if(solutions[i] == 0) {
+            cout << "INSOMNIA" << endl;
+        } else {
+            cout << solutions[i] << endl;
+        }
+    }
+}
+
+int main(int argc, char** args) {
     int numCases = 1;
+    cin >> numCases;
+
     int N[numCases];
     int solutions[numCases];
-    cout << "Input" << endl;
-    cin >> numCases;
 
     for(int i = 0; i < numCases; i++) {
         cin >> N[i];
     }
 
-    bool isInsomnia[numCases];
-    cout << "numCases: " << numCases << endl;
     for(int i = 0; i < numCases; i++) {
-        cout << "N[" << i << "]: " << N[i] << endl;
         solutions[i] = 0;
     }
 
-
     for(int i = 0; i < numCases; i++) {
-        cout << "Case: " << i+1 << endl;
         set<int> digitSet;
         set<int>::iterator it;
         if(N[i] == 0) {
@@ -46,26 +52,16 @@ int main() {
             for(int j = 1; digitSet.size() < 10; j++) {
                 int digit = j*N[i];
                 int numDigits = getNumDigits(digit);
-                cout << "digit: " << digit << endl;
 
                 for(int k = 0; k < numDigits; k++) {
                     int nthDigit = getNthDigit(digit, k);
                     digitSet.insert(nthDigit);
-                    cout << "Insert " << nthDigit << endl;
                 }
-                cout << "digitSetCount: " << digitSet.size() << endl;
                 solutions[i] = digit;
             }
         }
     }
 
-    for(int i = 0; i < numCases; i++) {
-        cout << "Case #" << i << ": ";
-        if(solutions[i] == 0) {
-            cout << "INSOMNIA" << endl;
-        } else {
-            cout << solutions[i] << endl;
-        }
-    }
+    printOutput(numCases, solutions);
 }
 
